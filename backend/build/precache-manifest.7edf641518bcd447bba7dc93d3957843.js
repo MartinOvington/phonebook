d7f01bf99bb3e5b9908a768aaef85474b0c8d951
Martin Ovington <MartinOvington@msn.com>
2022-06-29 09:51:57 +0100

    stopped git-ignoring build folder in backend folder

---
+++ backend/build/precache-manifest.7edf641518bcd447bba7dc93d3957843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24ff6f41ae34bc30905f1d16392ee72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077294f417dc654f4531",
+    "url": "/static/css/main.e1bc2bc0.chunk.css"
+  },
+  {
+    "revision": "e5abd4076c615cee7b15",
+    "url": "/static/js/2.a424ce89.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.a424ce89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077294f417dc654f4531",
+    "url": "/static/js/main.5b696fe0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
